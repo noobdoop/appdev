aa13fd56a22cd67d1f43166d4f6de260554a041c
noobdoop <116140554+noobdoop@users.noreply.github.com>
2024-02-06 15:59:29 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-from flask import Flask, render_template, request, jsonify, url_for, redirect
-
-from chat import get_response
-
+from flask import Flask, render_template, request, jsonify
 import shelve
 
 app = Flask(__name__)
@@ -18,7 +15,8 @@ data = [
         "reviews": [
             {"user": "bob", "comment": "very fun for my kids", "rating": 5},
             {"user": "jane", "comment": "bouncy bouncy", "rating": 4}
-        ]
+        ],
+        "tags":['Kids','Large','Obstacle Course'],
     },
     {
         "id": 2,
@@ -161,115 +159,10 @@ def repair():
 def sell():
     return render_template('sell.html')
 
-@app.route('/contactus')
+@app.route('/contact')
 def contact():
-    return render_template('contactus.html')
+    return render_template('contact.html')
 
-@app.route('/stickGame')
-def maze():
-    return render_template('stickGame.html')
-
-# chatbot stuff
-@app.route("/predict", methods= ["POST"])
-def predict():
-    text = request.get_json().get("message")
-    response = get_response(text)
-    message = {"answer": response}
-    return jsonify(message)
-
-@app.route('/get_conversation_data', methods=['GET'])
-def get_conversation_data():
-    with shelve.open('conversation_data.db') as db:
-        conversation_data = db.get('conversation', {})
-        message_data = conversation_data.get('message', {})
-        latest_msg_id = max(message_data.keys()) if message_data else 0
-        conversation_data['msgID'] = latest_msg_id
-    return jsonify(conversation_data)
-
-@app.route('/delete_conversation_data', methods=['POST'])
-def delete_conversation_data():
-    msg_id_to_delete = request.form.get('msgID')
-    with shelve.open('conversation_data.db', writeback=True) as db:
-        if 'conversation' in db and 'message' in db['conversation']:
-            message_data = db['conversation']['message']
-
-            try:
-                msg_id_to_delete = int(msg_id_to_delete)
-            except ValueError:
-                return "Invalid message ID provided."
-
-            if msg_id_to_delete in message_data:
-                del message_data[msg_id_to_delete]
-                print(f"Conversation Data with ID {msg_id_to_delete} Deleted")
-
-                remaining_msg_ids = list(message_data.keys())
-                remaining_msg_ids.sort()
-                for i, msg_id in enumerate(remaining_msg_ids, start=1):
-                    if msg_id != i:
-                        message_data[i] = message_data.pop(msg_id)
-                print("Remaining Message IDs After Shift:", list(message_data.keys()))
-
-                return "Conversation data deleted successfully"
-
-    print(f"No conversation data found with ID {msg_id_to_delete} to delete")
-    return "Failed to delete conversation data. Message ID not found."
-
-
-# feedback form stuff
-def get_feedback_data():
-    with shelve.open('feedback_db') as db:
-        return db.get('feedbacks', [])
-
-@app.route('/get_feedback_data')
-def get_feedback_data_route():
-    feedbacks = get_feedback_data()
-    return jsonify(feedbacks)
-
-@app.route('/submit_feedback', methods=['POST'])
-def submit_feedback():
-    try:
-        # Retrieve feedback data from JSON request
-        feedback_data = request.json
-
-        # Save feedback data to Shelve database
-        with shelve.open('feedback_db', writeback=True) as db:
-            if 'feedbacks' not in db:
-                db['feedbacks'] = []
-
-            db['feedbacks'].append(feedback_data)
-
-        print("Feedback data saved:", feedback_data)
-        return jsonify({"status": "success"})
-
-    except Exception as e:
-        print(f"Error submitting feedback: {str(e)}")
-        return jsonify({"status": "error"})
-
-
-@app.route('/feedback')
-def display_feedback():
-    # Retrieve feedback data from Shelve database
-    with shelve.open('feedback_db') as db:
-        feedbacks = db.get('feedbacks', [])
-
-    return render_template("feedback.html", feedbacks=feedbacks)
-
-@app.route('/delete_feedback/<int:index>')
-def delete_feedback(index):
-    try:
-        with shelve.open('feedback_db', writeback=True) as db:
-            if 'feedbacks' in db and 0 < index <= len(db['feedbacks']):
-                del db['feedbacks'][index - 1]
-                return redirect(url_for('display_feedback'))
-
-        return jsonify({"status": "error", "message": "Invalid index for deletion"})
-
-    except Exception as e:
-        print(f"Error deleting feedback: {str(e)}")
-        return jsonify({"status": "error"})
-
-
-# Game Stuff
 
 if __name__ == "__main__":
     app.run(debug=True)
